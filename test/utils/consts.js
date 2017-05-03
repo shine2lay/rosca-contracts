0efad5add8d08d4fa97ca125e5e0e09323bc46c8
shine2lay <aungshine@gmail.com>
2017-05-03 12:26:40 -0700

    WIP refactor part 2

--- test/utils/consts.js
+++ test/utils/consts.js
@@ -5,17 +5,23 @@ module.exports = {
   MAX_GAS_COST_PER_TX: 1e5 /* gas used per tx */ * 2e10, /* gas price */  // keep in sync with truffle.js
   ROUND_PERIOD_IN_SECS: 100,
   CONTRIBUTION_SIZE: 1e16,
-  MEMBER_LIST: function() {
+  memberList: function() {
     if(!memberList) {
-      console.log('Member list needs to be set first before calling MEMBER_LIST')
+      console.log('Member list needs to be set first before calling MEMBER_LIST');
+      process.exit(2);
     }
-    return memberList
+    return memberList;
   },
   setMemberList: function(accounts) {
     memberList = [accounts[1], accounts[2], accounts[3]]
   },
   MEMBER_COUNT: function() {
-    return memberList.length + 1
+    if(!memberList) {
+      console.log('Member list needs to be set first before calling MEMBER_COUNT');
+      process.exit(2);
+    }
+
+    return memberList.length + 1;
   },
   DEFAULT_POT : function() {
     return (this.CONTRIBUTION_SIZE * this.MEMBER_COUNT())
